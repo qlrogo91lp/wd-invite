b68f7e0e9d27adc485699d8b950eae967d356f7f
yjkim <qlrogo91lp@gmail.com>
2025-05-23 15:36:12 +0900

    ✨ firestore 실시간 댓글 테스트 버전

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
+import CommentBox from './components/CommentBox'
 
 function App() {
   return (
     <>
-      <h1 className='text-5xl text-red-500'>Vite + React</h1>
+      <CommentBox />
     </>
   )
 }
