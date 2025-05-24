38972f2fcd9e01ed3ed9476315b2d5ca9064e82a
yjkim <qlrogo91lp@gmail.com>
2025-05-24 19:31:29 +0900

    ➕ font, framer-motion 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import Header from '@components/Header'
 import CommentBox from './components/CommentBox'
+import OpeningMessage from '@components/OpeningMessage'
+import ScrollFadeIn from '@components/common/ScrollFadeIn'
 
 function App() {
   return (
-    <>
+    <main className='flex flex-col max-w-[440px] mx-auto'>
+      <Header />
+      <ScrollFadeIn>
+        <OpeningMessage />
+      </ScrollFadeIn>
       <CommentBox />
-    </>
+    </main>
   )
 }
 
