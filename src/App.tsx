aa9839b9f3a3acdb7704bedace45b541f1020e2b
yjkim <qlrogo91lp@gmail.com>
2025-06-09 14:30:36 +0900

    🎨 framer-motion 컴포넌트 마다 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Header from '@components/Header'
 import OpeningMessage from '@components/OpeningMessage'
-import ScrollFadeIn from '@components/common/ScrollFadeIn'
 import CustomCountdown from '@components/countdown/CustomCountdown'
 import Calendar from '@components/Calendar'
 import FamilyNames from '@components/FamliyNames'
@@ -10,16 +9,14 @@ import CustomGallery from '@components/CustomGallery'
 
 function App() {
   return (
-    <main className='flex flex-col max-w-[440px] mx-auto'>
+    <main className='flex flex-col max-w-[440px] mx-auto gap-15'>
       <Header />
-      <ScrollFadeIn>
-        <OpeningMessage />
-        <FamilyNames />
-        <CustomGallery />
-        <CustomCountdown />
-        <Calendar />
-        <Directions />
-      </ScrollFadeIn>
+      <OpeningMessage />
+      <FamilyNames />
+      <CustomGallery />
+      <CustomCountdown />
+      <Calendar />
+      <Directions />
       <Footer />
     </main>
   )
