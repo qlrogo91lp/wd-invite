059803db293ec6bad4cd2bd3ea01c2717d2dc7c1
yjkim <qlrogo91lp@gmail.com>
2025-06-11 09:47:09 +0900

    ✨ 카카오 공유하기 기능 추가

--- src/global.d.ts
+++ src/global.d.ts
@@ -2,6 +2,9 @@ declare global {
   interface Window {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     naver: any;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    Kakao: any;
   }
 }
+
 export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,32 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@icons': path.resolve(__dirname, './src/assets/icons'),
-      '@svgs': path.resolve(__dirname, './src/assets/svgs'),
-      '@utils': path.resolve(__dirname, './src/utils'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react(), tailwindcss(), visualizer()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@icons': path.resolve(__dirname, './src/assets/icons'),
+        '@svgs': path.resolve(__dirname, './src/assets/svgs'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      },
     },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('firebase')) return 'firebase';
-          if (id.includes('firebase')) return 'firebase';
-          if (id.includes('framer-motion')) return 'motion';
-          if (id.includes('react-spinners')) return 'spinners';
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks(id) {
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('firebase')) return 'firebase';
+            if (id.includes('framer-motion')) return 'motion';
+            if (id.includes('react-spinners')) return 'spinners';
+          }
         }
       }
+    },
+    define: {
+      'process.env': env
     }
   }
 })
